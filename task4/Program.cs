e54e4ae2af289685f2f6deddc5f298fd42f91eba
ekaterina <katerina_w@icloud.com>
2022-06-21 19:57:51 +0300

    Задача на координаты 4

---
+++ task4/Program.cs
@@ -0,0 +1,12 @@
+﻿/*Напишите программу, которая принимает на вход число (N) и 
+выдаёт таблицу квадратов чисел от 1 до N.*/
+
+Console.WriteLine("Введите число");
+int n = int.Parse(Console.ReadLine());
+
+int i = 1;
+while(i <= n)
+{
+    Console.WriteLine(Math.Pow(i,2));
+    i++;
+}
