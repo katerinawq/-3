6662914c0ebae2b52c7a53aaf918993798e2d12f
ekaterina <katerina_w@icloud.com>
2022-06-21 18:14:23 +0300

    Задача на коррдинаты 2

---
+++  task2/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных 
+координат точек в этой четверти (x и y)*/
+
+Console.WriteLine("Напишите номер четверти от 1 до 4");
+int a = int.Parse(Console.ReadLine());
+
+if (a==1)
+{
+    Console.WriteLine("x > 0 и y > 0");
+}
+ else if (a==2) 
+ {
+    Console.WriteLine("x < 0 и y > 0");
+ }
+ else if (a==3) 
+ {
+    Console.WriteLine("x < 0 и y < 0");
+ }
+ else if (a==4) 
+ {
+    Console.WriteLine("x > 0 и y < 0");
+ }
+ else
+ {
+   Console.WriteLine("Введен не верный номер");
+}
+ 
