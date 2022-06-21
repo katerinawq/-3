21cce3184dece53a374ec989fe6b1fb648f636a0
ekaterina <katerina_w@icloud.com>
2022-06-21 17:15:39 +0300

    Задача на коррдинаты 1

---
+++ task1/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Напишите программу, которая принимает на вход координаты 
+точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти 
+плоскости, в которой находится эта точка.*/
+  
+  Console.WriteLine("Введите x");
+  int x = int.Parse(Console.ReadLine());
+
+  Console.WriteLine("Введите y");
+  int y = int.Parse(Console.ReadLine());
+
+  if (x > 0 & y >0)
+  {
+    Console.WriteLine("Первая плоскость");
+  }
+   else if (x < 0 & y < 0 )
+   {
+    Console.WriteLine("Вторая плоскость");
+   }
+   else if (x > 0 & y < 0 )
+   {
+    Console.WriteLine("Третья плоскость");
+   }
+   else 
+   {Console.WriteLine("Координаты не принадлежат плоскости");}
